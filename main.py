a6525cdfcf0037c7634b634bbe81766ffb6b7a3e
Kenneth Chew <kennethchew_11@otmail.com>
2019-07-30 20:49:58 +0800

    update main

--- main.py
+++ main.py
@@ -140,10 +140,10 @@ def Discriminator():
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
 # Adam optimizers
-generator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
-discriminator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
-#generator_optimizer = tf.optimizers.Adam(2e-4, beta_1=0.5)
-#discriminator_optimizer = tf.optimizers.Adam(2e-4, beta_1=0.5)
+#generator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
+#discriminator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
+generator_optimizer = tf.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.optimizers.Adam(2e-4, beta_1=0.5)
 
 #Create instance of generator and discriminator    
 generator = Generator()
