9585614953b60bf0ac22e86f35b088cdcb8b30c4
Kenneth Chew <kennethchew_11@otmail.com>
2019-08-01 09:05:28 +0800

    change training=False

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def generator_loss(disc_generated_output, gen_output, target):
 
 def train_step(input_image, target):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        gen_output = generator(input_image, training=True)
+        gen_output = generator(input_image, training=False)
 
         disc_real_output = discriminator([input_image, target], training=True)
         disc_generated_output = discriminator([input_image, gen_output], training=True)
